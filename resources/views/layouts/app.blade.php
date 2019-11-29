b5ff154f941456466a76cce850cab841edfeb2b0
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-11-29 07:01:56 -1000

    request controller, installed guzzle

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>RequestMan</title>
+</head>
+<body>
+    @include('inc.navbar')
+    @include('inc.messages')
+    @yield('content')
+</body>
+</html>
